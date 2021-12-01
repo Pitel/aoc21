fe4b807addaefc7a7c7ced684ed9a2ba2bb8cab8
Ing. Jan Kaláb <jan+git@kalabovi.org>
2021-12-01 13:31:38 +0100

    Simnplify even more

--- day01/src/main/kotlin/com/24i/adventofcode/App.kt
+++ day01/src/main/kotlin/com/24i/adventofcode/App.kt
@@ -1,17 +1,7 @@
 package com.`24i`.adventofcode
 
 val List<UInt>.increases
-    get() = foldIndexed(0u) { i, acc, depth ->
-        if (i == 0) {
-            acc
-        } else {
-            if (this[i - 1] < depth) {
-                acc.inc()
-            } else {
-                acc
-            }
-        }
-    }
+    get() = windowed(2).count { (a, b) -> a < b }
 
 fun main() {
     val input = object {}.javaClass.getResource("/input.txt").readText()
